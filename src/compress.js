0440442faa9fdc49271aed1a8ab6461c573f7131
DemonGodXY <gfxks457@proton.me>
2025-08-20 12:34:29 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -6,18 +6,17 @@
 */
 const sharp = require('sharp')
 const redirect = require('./redirect')
-const { PassThrough } = require('stream');
+const { PassThrough, Transform } = require('stream');
 const sharpStream = _ => sharp({ animated: !process.env.NO_ANIMATE, unlimited: true });
 sharp.cache(false)
 
 function compress(req, res, input) {
   const format = req.params.webp ? 'webp' : 'jpeg'
   
-  // Set all headers before streaming begins
+  // Set headers and announce trailers
   res.setHeader('cache-control', 'public, max-age=604800, stale-while-revalidate=86400')
   res.setHeader('content-type', 'image/' + format);
-  res.setHeader('x-original-size', req.params.originSize);
-  // Note: We can't set x-bytes-saved because we don't know the final size yet
+  res.setHeader('Trailer', 'x-original-size, x-bytes-saved');
 
   const transform = sharpStream()
     .grayscale(req.params.grayscale)
@@ -27,16 +26,32 @@ function compress(req, res, input) {
       optimizeScans: true
     });
 
+  let totalSize = 0;
   const passThrough = new PassThrough();
+  const counter = new Transform({
+    transform(chunk, encoding, callback) {
+      totalSize += chunk.length;
+      this.push(chunk);
+      callback();
+    }
+  });
 
   input.body
     .on('error', () => req.socket.destroy())
     .pipe(transform)
     .on('error', () => redirect(req, res))
+    .pipe(counter)
     .pipe(passThrough)
     .on('error', () => redirect(req, res));
 
-  // Start streaming immediately
+  // Set trailers when stream ends
+  passThrough.on('end', () => {
+    res.addTrailers({
+      'x-original-size': req.params.originSize,
+      'x-bytes-saved': req.params.originSize - totalSize
+    });
+  });
+
   passThrough.pipe(res);
 }
 
