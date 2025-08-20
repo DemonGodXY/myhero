e523c3415a4662d3ce396978acfafcdf898fc265
DemonGodXY <gfxks457@proton.me>
2025-08-20 12:32:39 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -6,22 +6,18 @@
 */
 const sharp = require('sharp')
 const redirect = require('./redirect')
-const { PassThrough, Transform } = require('stream');
+const { PassThrough } = require('stream');
 const sharpStream = _ => sharp({ animated: !process.env.NO_ANIMATE, unlimited: true });
 sharp.cache(false)
 
 function compress(req, res, input) {
   const format = req.params.webp ? 'webp' : 'jpeg'
-  /*
-  * input.pipe => sharp (The compressor) => Send to httpResponse
-  * The following headers:
-  * | Header Name | Description | Value |
-  * |---------------|-----------------------------------|----------------------------|
-  * |x-original-size|Original photo size |OriginSize |
-  * |x-bytes-saved |Saved bandwidth from original photo|OriginSize - Compressed Size|
-  */
+  
+  // Set all headers before streaming begins
   res.setHeader('cache-control', 'public, max-age=604800, stale-while-revalidate=86400')
   res.setHeader('content-type', 'image/' + format);
+  res.setHeader('x-original-size', req.params.originSize);
+  // Note: We can't set x-bytes-saved because we don't know the final size yet
 
   const transform = sharpStream()
     .grayscale(req.params.grayscale)
@@ -31,29 +27,16 @@ function compress(req, res, input) {
       optimizeScans: true
     });
 
-  let totalSize = 0;
   const passThrough = new PassThrough();
-  const counter = new Transform({
-    transform(chunk, encoding, callback) {
-      totalSize += chunk.length;
-      this.push(chunk);
-      callback();
-    }
-  });
 
   input.body
     .on('error', () => req.socket.destroy())
     .pipe(transform)
     .on('error', () => redirect(req, res))
-    .pipe(counter)
     .pipe(passThrough)
     .on('error', () => redirect(req, res));
 
-  passThrough.on('end', () => {
-    res.setHeader('x-original-size', req.params.originSize);
-    res.setHeader('x-bytes-saved', req.params.originSize - totalSize);
-  });
-
+  // Start streaming immediately
   passThrough.pipe(res);
 }
 
